f3486f4aa5bfea038f12f18ec50059418fbd97e4
moguy93 <guymorgan@live.fr>
2024-09-30 11:16:24 +0200

    add link jitsi + correction

---
+++ corrections/exercice2.py
@@ -0,0 +1,66 @@
+"""
+Exercice 1 : Filtrage et transformation avec les listes en compréhension
+
+Énoncé
+Vous disposez d'une liste de nombres entiers de 1 à 20.
+Vous devez créer une nouvelle liste qui contient le carré de chaque nombre pair de la liste initiale.
+
+Solution
+"""
+# Étape 1 : Créer la liste initiale de 1 à 20
+
+# Étape 2 : Utiliser une liste en compréhension pour filtrer les nombres pairs et calculer leur carré
+
+# Étape 3 : Afficher la liste résultante
+
+
+"""
+Exercice 2 : Recherche avec for-else
+
+Énoncé
+Vous devez écrire un programme qui vérifie si un nombre donné est un nombre premier.
+Si le nombre est premier, affichez un message confirmant qu'il est premier.
+Sinon, affichez son plus petit diviseur autre que 1.
+
+Solution
+"""
+# Étape 1 : Demander à l'utilisateur d'entrer un nombre entier positif supérieur à 1
+
+# Vérifier que l'entrée est valide
+
+    # Étape 2 : Utiliser une boucle for-else pour vérifier si le nombre est premier
+            # Si un diviseur est trouvé, le nombre n'est pas premier
+        # Si aucun diviseur n'est trouvé, le nombre est premier
+
+"""
+Exercice 3 : Utilisation de l'opérateur ternaire
+
+Énoncé
+Écrivez un programme qui demande à l'utilisateur son âge et détermine s'il est majeur ou mineur en utilisant l'opérateur ternaire.
+
+Solution
+"""
+# Étape 1 : Demander à l'utilisateur d'entrer son âge
+
+# Étape 2 : Utiliser l'opérateur ternaire pour déterminer le statut
+
+# Étape 3 : Afficher le statut
+
+"""
+Exercice 4 : Combinaison des trois concepts
+
+Énoncé
+Créez un programme qui génère une liste des 10 premiers nombres entiers positifs.
+Pour chaque nombre, si le nombre est divisible par 2, ajoutez son carré à la liste, sinon, ajoutez son cube.
+Utilisez une liste en compréhension avec un opérateur ternaire pour cela.
+Ensuite, vérifiez si le nombre 100 est présent dans la liste résultante en utilisant une boucle for-else.
+
+Solution
+"""
+# Étape 1 : Générer la liste des nombres entiers de 1 à 10
+
+# Étape 2 : Utiliser une liste en compréhension avec un opérateur ternaire
+
+# Étape 3 : Afficher la liste résultante
+
+# Étape 4 : Utiliser une boucle for-else pour vérifier si 100 est dans la liste
