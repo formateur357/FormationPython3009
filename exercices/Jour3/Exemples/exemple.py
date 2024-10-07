08717b53ead125504ff1f7758f3a6d7f2c4cffe5
moguy93 <guymorgan@live.fr>
2024-10-07 10:09:05 +0200

    add exemple

---
+++ exercices/Jour3/Exemples/exemple.py
@@ -0,0 +1,35 @@
+x = 10  # Correct
+x=10  # Incorrect
+
+appel_fonction(x, y, z) # Correct
+appel_fonction( x, y, z ) # Incorrect
+
+# Correct
+nombre_utilisateurs = 10
+def calculer_moyenne():
+  pass 
+
+# Incorrect
+NombreUtilisateurs = 10
+def CalculerMoyenne():
+  pass
+
+# Correct
+class UtilisateurActif:
+  pass
+
+# Correct
+MAX_UTILISATEURS = 100
+
+# Correct
+import os # Modules standards
+import sys
+
+import numpy # Modules tiers
+
+import fibonnaci # Modules locaux
+
+# Incorrect
+import os, sys
+
+#
