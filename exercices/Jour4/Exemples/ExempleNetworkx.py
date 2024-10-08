03c74df9d0cbdc6074da8593454327ebc0f82d43
moguy93 <guymorgan@live.fr>
2024-10-08 10:09:10 +0200

    add exmple

--- exercices/Jour4/Exemples/ExempleNetworkx.py
+++ exercices/Jour4/Exemples/ExempleNetworkx.py
@@ -33,3 +33,12 @@ nx.draw(
 # Ajouter un titre
 plt.title("Graphe de Données Liées")
 plt.show()  # Afficher le graphique
+
+"""
+Explication du code
+Création des données : Un DataFrame est créé à partir d'un dictionnaire représentant les relations entre différentes personnes.
+
+Création du graphe : Utilisez nx.from_pandas_edgelist pour générer un graphe orienté à partir des relations définies dans le DataFrame.
+
+Visualisation : nx.draw est utilisé pour visualiser le graphe. Les paramètres comme node_size, node_color, et arrows permettent de personnaliser l'apparence du graphe.
+"""
